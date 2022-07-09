966a5e410c75e97320b591629fd565370fc62694
zjjnnn <qthyzjj@gmail.com>
2022-07-09 10:15:35 +0900

    クレジットカード変更

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <router-link to="/goods/detail/10195">商品ページ 10195</router-link> |
     <router-link to="/my-account/wishlist">お気に入り</router-link> |
     <router-link to="/my-account/profileEditSelect">情報変更</router-link> |
-    <router-link to="/my-account/address-book">配送先住所</router-link>
+    <router-link to="/my-account/address-book">配送先住所</router-link> |
+    <router-link to="/my-account/payment-details">クレジットカード</router-link>
   </p>
   <router-view></router-view>
 </template>
---
+++ src/pages/myaccount/PaymentDetails.vue
@@ -0,0 +1,9 @@
+<template>
+  <payment-container />
+</template>
+
+<script setup lang="ts">
+import PaymentContainer from "../../components/payment/PaymentContainer.vue";
+</script>
+
+<style scoped></style>
